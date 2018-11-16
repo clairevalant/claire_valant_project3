d001309682abb4b6f91ac6e640d1da48e194c4e6
clairevalant <valantclaire@gmail.com>
2018-11-15 20:02:38 -0500

    work on results array

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+// Script for Claire Valant Project 3: Toronto Tattoo Tinder
+
+// when an input is selected, add a class of the label to "selected" to change the color to #ff616f
+
+
+// when the form is submitted,
+tattApp.formSubmit = function(){
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+
+        // get values from inputs, assign them to constants
+        const userType = $("input[name=type]:checked").val();
+        const userMeaning = $("input[name=meaning]:checked").val();
+        const userSize = $("input[name=size]:checked").val();
+        const userStyle = $("input[name=style]:checked").val();
+        const userColour = $("input[name=colour]:checked").val();
+        const userCost = $("input[name=cost]:checked").val();
+    
+        // check that all questions are filled out
+        if (userType == undefined ||
+            userMeaning == undefined ||
+            userSize == undefined ||
+            userStyle == undefined ||
+            userColour == undefined ||
+            userCost == undefined) {
+                // show an error message (slide down shows it)
+        } else {
+            // pass results to filter tattoo array
+            tattApp.findResults(userType, userMeaning, userSize, userStyle, userColour, userCost);
+        }
+    });
+}
+
+// filter allTattoos array for only objects with properties relevant to user
+tattApp.findResults = function(uType, uMeaning, uSize, uStyle, uColour, uCost) {
+    const results = tattApp.allTattoos.filter(function(tatt){
+        return tatt.type === uType && tatt.meaning === uMeaning && tatt.size === uSize && tatt.style === uStyle && tatt.colour === uColour && tatt.cost === uCost
+    })
+
+    console.log(results);
+    
+    // randomly select one tattoo idea for the user and one artist from the list
+    // const rand = Math.floor(Math.random * results.length);
+    // const finalAnswer = results.rand;
+}
+
+
+
+// share results with user! pop up? shuffle in from the right?
+
+// initiating function
+tattApp.init = function() {
+    // All methods go here
+    tattApp.formSubmit();
+}
+
+// document ready
+$(function() {
+    tattApp.init();
+});
